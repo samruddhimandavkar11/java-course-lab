d9f9ff85a7a0cc73c5c9baa82ea5e17f80d7fa1e
samruddhimandavkar11 <mandavkarsamruddhi3792@gmail.com>
2025-07-27 19:32:56 +0530

    lab7

---
+++ LAB7/src/SumAndAverage.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SumAndAverage {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int size = sc.nextInt();
+
+        int[] numbers = new int[size];
+        int sum = 0;
+
+        System.out.println("Enter " + size + " integers:");
+        for(int i = 0; i < size; i++) {
+            numbers[i] = sc.nextInt();
+            sum += numbers[i];
+        }
+
+        double average = (double) sum / size;
+
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + average);
+    }
+}
